91e0d86521d7becccb91d6970adfc39b25117fa8
Doug Lloyd <lloyd@cs50.harvard.edu>
2018-09-20 08:16:32 -0400

    EJS

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const hbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const _ = require('lodash');
+const path = require('path');
 require('cross-fetch/polyfill');
 
 const app = express();
 const host = '127.0.0.1';
 const port = 3000;
 
-app.engine('handlebars', hbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
-app.use('/css', express.static('public/css'));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 const API_KEY = '43a19a70-b76e-11e8-bf0e-e9322ccde4db';
 
@@ -17,7 +18,7 @@ app.get('/', (req, res) => {
   fetch(url)
   .then(response => response.json())
   .then(data => {
-    res.render('home', {title: "Gallery Listing", galleries: data.records});
+    res.render('index', {galleries: data.records});
   });
 });
 
@@ -26,7 +27,7 @@ app.get('/gallery/:gallery_id', function(req, res) {
   fetch(url)
   .then(response => response.json())
   .then(data => {
-    res.render('objects', {title: "Object Listing", gallery: req.params.gallery_id, objects: data.records});
+    res.render('objects', {gallery: req.params.gallery_id, objects: data.records});
   });
 });
 
@@ -35,7 +36,7 @@ app.get('/object/:object_id', function(req, res) {
   fetch(url)
   .then(response => response.json())
   .then(data => {
-    res.render('object', {title: "Artifact Information", object: data});
+    res.render('object', {object: data});
   });
 });
 
